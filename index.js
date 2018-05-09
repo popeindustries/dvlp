038948e89f94b7a3042f2b2f9d6f1434baad619b
Alexander Pope <alex@pope-industries.com>
2018-05-09 11:50:58 +0200

    add reload server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-exports.testServer = require('./lib/testServer').create;
+'use strict';
+
+const server = require('./lib/server');
+const testServer = require('./lib/testServer');
+
+module.exports = {
+  server,
+  testServer
+};
---
+++ test/reloadServer-test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const { client: WSClient } = require('websocket');
+const { expect } = require('chai');
+const fetch = require('node-fetch');
+const reloadServer = require('../lib/ReloadServer');
+
+let server;
+
+describe('ReloadServer', () => {
+  before(async () => {
+    server = await reloadServer();
+  });
+  after(() => {
+    if (server) {
+      server.destroy();
+    }
+  });
+
+  it('should serve the livereload.js file with correct mime type', async () => {
+    const res = await fetch('http://localhost:35729/livereload.js');
+    expect(res.status).to.eql(200);
+    expect(res.headers.get('Content-type')).to.eql('text/javascript');
+  });
+  it('should return 404 for all other content', async () => {
+    const res = await fetch('http://localhost:35729/nothing');
+    expect(res.status).to.eql(404);
+  });
+  it('should open a socket connection', (done) => {
+    const client = new WSClient();
+    client.on('connect', () => {
+      client.abort();
+      done();
+    });
+    client.connect('ws://localhost:35729');
+  });
+});
