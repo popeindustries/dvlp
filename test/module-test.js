0dc2112d579e6d359d1ef9d5ed63e51cdbfe2b30
Alexander Pope <alex@pope-industries.com>
2018-05-18 13:28:57 +0200

    refactor utils

---
+++ test/module-test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const { expect } = require('chai');
+const { CACHE_DIR, clearCache, bundle } = require('../lib/utils/module');
+const path = require('path');
+
+const LODASH = 'lodash-4.17.10.js';
+
+describe('module', () => {
+  afterEach(clearCache);
+
+  describe('bundle()', () => {
+    it('should return "null" if no module bundle found', () => {
+      expect(bundle('foofoo')).to.equal(null);
+    });
+    it('should bundle and return bundle filepath', async () => {
+      const filepath = await bundle('lodash');
+      expect(filepath).to.equal(path.join(CACHE_DIR, LODASH));
+    });
+    it('should return cached bundle filepath', async () => {
+      await bundle('lodash');
+      const filepath = await bundle('lodash');
+      expect(filepath).to.equal(path.join(CACHE_DIR, LODASH));
+    });
+  });
+});
--- test/patch-test.js
+++ test/patch-test.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const { expect } = require('chai');
+const { clearCache } = require('../lib/utils/module');
 const { ServerResponse } = require('http');
-const { patchRequest, patchResponse } = require('../lib/patch');
+const { patchRequest, patchResponse } = require('../lib/utils/patch');
 
 function getBody(res) {
   const output = res.output.filter((chunk) => typeof chunk === 'string').join('');
@@ -19,6 +20,8 @@ function getRequest(url, headers = { accept: '*/*' }) {
 }
 
 describe('patch', () => {
+  afterEach(clearCache);
+
   describe('patchRequest()', () => {
     it('should add correct type', () => {
       const req = getRequest('index.js');
@@ -51,12 +54,21 @@ describe('patch', () => {
         '<script src="http://localhost:35729/livereload.js"></script>\n</body>'
       );
     });
-    it.only('should resolve bare js import path', () => {
+    it('should resolve bare js import id', () => {
       const req = getRequest('index.js', { accept: 'application/javascript' });
       const res = new ServerResponse(req);
       patchResponse(req, res, true);
       res.end('import "lodash";');
-      expect(getBody(res)).to.include('/.dvlp/lodash-4.17.10.js');
+      expect(getBody(res)).to.equal('import "/.dvlp/lodash-4.17.10.js";');
+    });
+    it('should resolve multiple bare js import ids', () => {
+      const req = getRequest('index.js', { accept: 'application/javascript' });
+      const res = new ServerResponse(req);
+      patchResponse(req, res, true);
+      res.end('import "lodash/array";\nimport "./foo.js";\nimport "debug";');
+      expect(getBody(res)).to.equal(
+        'import "/.dvlp/lodash%2Farray-4.17.10.js";\nimport "./foo.js";\nimport "/.dvlp/debug-3.1.0.js";'
+      );
     });
   });
 });
