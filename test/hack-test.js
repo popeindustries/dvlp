94c21154ffda8e0879c055bd667ef427b815945d
Alexander Pope <alex@pope-industries.com>
2018-06-21 09:00:51 +0200

    rename

--- test/hack-test.js
+++ test/hack-test.js
@@ -3,7 +3,7 @@
 const { cleanBundles, destroyWorkers } = require('../lib/utils/bundler');
 const { expect } = require('chai');
 const { bundleDirName } = require('../lib/config');
-const { patchResponse } = require('../lib/utils/patch');
+const { hackResponse } = require('../lib/utils/hack');
 const path = require('path');
 const { ServerResponse } = require('http');
 
@@ -23,7 +23,7 @@ function getRequest(url, headers = { accept: '*/*' }) {
   };
 }
 
-describe('patch', () => {
+describe('hack', () => {
   before(() => {
     process.env.NODE_PATH = 'test/fixtures/www';
     require('module').Module._initPaths();
@@ -37,32 +37,32 @@ describe('patch', () => {
     await destroyWorkers();
   });
 
-  describe('patchResponse()', () => {
+  describe('hackResponse()', () => {
     it('should inject script into buffered html response', () => {
       const req = getRequest('index.html', { accept: 'text/html' });
       const res = new ServerResponse(req);
-      patchResponse(req, res, { scriptString: 'test inject' });
+      hackResponse(req, res, { scriptString: 'test inject' });
       res.end('</body>');
       expect(getBody(res)).to.include('test inject');
     });
     it('should inject script into streamed html response', () => {
       const req = getRequest('index.html', { accept: 'text/html' });
       const res = new ServerResponse(req);
-      patchResponse(req, res, { scriptString: 'test inject' });
+      hackResponse(req, res, { scriptString: 'test inject' });
       res.write('</body>');
       expect(getBody(res)).to.include('test inject');
     });
     it('should resolve bare js import id', () => {
       const req = getRequest('index.js', { accept: 'application/javascript' });
       const res = new ServerResponse(req);
-      patchResponse(req, res);
+      hackResponse(req, res);
       res.end('import lodash from "lodash";');
       expect(getBody(res)).to.equal(`import lodash from "/${bundleDirName}/lodash-4.17.10.js";`);
     });
     it('should resolve multiple bare js import ids', () => {
       const req = getRequest('index.js', { accept: 'application/javascript' });
       const res = new ServerResponse(req);
-      patchResponse(req, res);
+      hackResponse(req, res);
       res.end(
         'import lodashArr from "lodash/array";\nimport { foo } from "./foo.js";\nimport debug from "debug";'
       );
@@ -74,7 +74,7 @@ describe('patch', () => {
       const req = getRequest('index.js', { accept: 'application/javascript' });
       req.filepath = path.resolve('test/fixtures/index.js');
       const res = new ServerResponse(req);
-      patchResponse(req, res);
+      hackResponse(req, res);
       res.end('import module from "nested/index.js";');
       expect(getBody(res)).to.equal(`import module from "../www/nested/index.js";`);
       process.env.NODE_PATH = NODE_PATH;
@@ -82,14 +82,14 @@ describe('patch', () => {
     it('should resolve js import id missing extension', () => {
       const req = getRequest('index.js', { accept: 'application/javascript' });
       const res = new ServerResponse(req);
-      patchResponse(req, res);
+      hackResponse(req, res);
       res.end('import module from "./test/fixtures/www/module";');
       expect(getBody(res)).to.equal(`import module from "./test/fixtures/www/module.js";`);
     });
     it('should resolve js import id missing package index', () => {
       const req = getRequest('index.js', { accept: 'application/javascript' });
       const res = new ServerResponse(req);
-      patchResponse(req, res);
+      hackResponse(req, res);
       res.end('import module from "./test/fixtures/www/nested";');
       expect(getBody(res)).to.equal(`import module from "./test/fixtures/www/nested/index.js";`);
     });
