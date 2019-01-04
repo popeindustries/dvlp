f4f90e0c576afde639683d44d4199c750f3b9af3
Alexander Pope <alex@pope-industries.com>
2019-01-04 09:20:00 +0100

    upgrade dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,8 @@ const resolve = require('rollup-plugin-node-resolve');
 module.exports = {
   external: ['worker-farm', 'fswatcher-child', 'esm'],
   input: 'lib/index.js',
-  plugins: [commonjs(), json(), resolve()],
+  // Fix error with rollup resolving of acorn by ignoring 'module'
+  plugins: [commonjs(), resolve({ module: false }), json()],
   output: {
     file: 'dvlp.js',
     format: 'cjs'
