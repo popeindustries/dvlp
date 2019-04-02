157e51291c428d80106c5aba9d11f0d3f201c24d
Alexander Pope <alex@pope-industries.com>
2019-04-02 08:25:26 +0200

    create mock package

--- test/mock-test.js
+++ test/mock-test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { expect } = require('chai');
-const Mock = require('../lib/test-server/mock.js');
+const Mock = require('../lib/mock/mock.js');
 
 const mocks = new Mock();
 
