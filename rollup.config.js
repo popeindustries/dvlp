24410ef7df1019bce96bf37636c7a4f54ed68ea7
Alexander Pope <alex@pope-industries.com>
2019-04-05 13:34:25 +0200

    bundle bundle-worker.js

--- rollup.config.js
+++ rollup.config.js
@@ -9,21 +9,36 @@ const terser = require('terser');
 const reloadClient = terser.minify(
   fs.readFileSync('lib/reloader/reload-client.js', 'utf8')
 ).code;
+const mockClient = terser
+  .minify(fs.readFileSync('lib/mock/mock-client.js', 'utf8'))
+  .code.replace(/'/g, "\\'");
 
-module.exports = {
-  external: [...builtinModules, 'worker-farm', 'fswatcher-child'],
-  input: 'lib/index.js',
-  plugins: [
-    replace({
-      'global.$RELOAD_CLIENT': `'${reloadClient}'`
-    }),
-    commonjs(),
-    // Fix error with rollup resolving of acorn by ignoring 'module'
-    resolve({ module: false }),
-    json()
-  ],
-  output: {
-    file: 'dvlp.js',
-    format: 'cjs'
+module.exports = [
+  {
+    external: [...builtinModules],
+    input: 'lib/bundler/bundle-worker.js',
+    plugins: [commonjs(), resolve({ module: false }), json()],
+    output: {
+      file: 'bundle-worker.js',
+      format: 'cjs'
+    }
+  },
+  {
+    external: [...builtinModules, 'worker-farm', 'fswatcher-child'],
+    input: 'lib/index.js',
+    plugins: [
+      replace({
+        'global.$RELOAD_CLIENT': `'${reloadClient}'`,
+        'global.$MOCK_CLIENT': `'${mockClient}'`
+      }),
+      commonjs(),
+      // Fix error with rollup resolving of acorn by ignoring 'module'
+      resolve({ module: false }),
+      json()
+    ],
+    output: {
+      file: 'dvlp.js',
+      format: 'cjs'
+    }
   }
-};
+];
