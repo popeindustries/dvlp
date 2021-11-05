c072341a85570be758eb5e691778a0b1fc94bdaf
Alexander Pope <alex@pope-industries.com>
2021-11-05 13:47:01 +0100

    add pnpm link test

--- test/unit/resolver-test.js
+++ test/unit/resolver-test.js
@@ -197,6 +197,11 @@ describe('resolver', () => {
       it('should resolve a symlinked js package file', () => {
         expect(resolve('linked', path.resolve('baz.js'))).to.equal(path.resolve('linked/index.js'));
       });
+      it.only('should resolve a pnpm style symlinked js package file', () => {
+        expect(resolve('a', path.resolve('baz.js'))).to.equal(
+          path.resolve('node_modules/.pnpm/a@1.0.0/node_modules/a/index.js'),
+        );
+      });
     }
     it('should resolve package exports entry', () => {
       expect(resolve('exports', path.resolve('baz.js'))).to.equal(path.resolve('node_modules/exports/browser.js'));
